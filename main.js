4781ce1d4bcd4631636aa4525dcc72ad953fba72
Jack Barker <boynamedjack@Jacks-MacBook-Pro.local>
2024-10-08 08:39:22 -0600

    following provided instructions to give setup for project. Have API pulls coming through and the containers to display the data

--- main.js
+++ main.js
@@ -16,3 +16,26 @@ function getAvailableGolfCourses() {
     });
   }
   
+  let courseOptionsHtml = '';
+courses.forEach((course) => {
+ courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+});
+
+document.getElementById('course-select').innerHTML = courseOptionsHtml;
+
+let teeBoxSelectHtml = ''
+teeBoxes.forEach(function (teeBox, index) {
+   teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
+     teeBox.totalYards
+   } yards</option>`
+});
+
+document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
+
+class Player {
+    constructor(name, id = getNextId(), scores = []) {
+      this.name = name;
+      this.id = id;
+      this.scores = scores;
+    }
+  }
